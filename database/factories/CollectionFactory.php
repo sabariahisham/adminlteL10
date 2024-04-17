4a00601b8b1000b948b611db61cc7901e829a299
Razali Mohamad <razali.mohamad@treasury.gov.my>
2024-04-17 15:57:52 +0800

    repo-upd

---
+++ database/factories/CollectionFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Collection;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Collection>
+ */
+class CollectionFactory extends Factory
+{
+    protected $model = Collection::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'slug' => $this->faker->slug,
+            'image' => 'admin/dist/img/avatar.png',
+            'pdf' => 'admin/pdf/dummy.pdf',
+            'category_id' => $this->faker->numberBetween(1, 10),
+        ];
+    }
+}
